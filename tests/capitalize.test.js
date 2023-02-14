c07f10e0111ff277c38d925e201404abcf23e99a
Danil Volkov <volchara287@gmail.com>
2023-02-15 00:19:09 +0600

    Заменил ручное тесстирование на assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,18 @@
 import capitalize from "../src/capitalize.js";
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.strictEqual(capitalize('hello'), 'Hello') // Для сравнения двух значений 
+// Первый параметр actual – то, что пришло
+// Второй параметр expected – то, что ожидает тест
+// Сравнивает также ссылки на объекты, т.е. два одиннаковых массива могут
+// не подойти под это условие
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.deepEqual(capitalize('hello'), 'Hello') // Упирается только на содержимое
+// Не сравнивает ссылки
+
+
+// Используются для негативных сравнений, где объекты не равны
+// assert.notStrictEqual(capitalize('hello'), 'Hello') 
+// assert.notDeepStrictEqual(capitalize('hello'), 'Hello')
 
 console.log('Все тесты пройдены!');
